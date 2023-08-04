2437bcbab1f65a374721042bbe28ef63303d98f8
spandanakoppaku <skoppaku@gmail.com>
2023-08-04 13:50:55 -0400

    Auto Push

--- mysql.tf
+++ mysql.tf
@@ -1,13 +1,14 @@
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_name              = "roboshop-${var.ENV}-mysql"
-  username             = "foo"
-  password             = "bar"
-  db_subnet_group_name = aws_db_subnet_group.mysql_subnet_group.name
-  parameter_group_name = aws_db_parameter_group.mysql_pg.name
+  allocated_storage       = 10
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t2.micro"
+  username                = "admin1"
+  password                = "roboshop1"
+  db_subnet_group_name    = aws_db_subnet_group.mysql_subnet_group.name
+  parameter_group_name    = aws_db_parameter_group.mysql_pg.name
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = aws_security_group.allows_mysql.id
 }
 
 # Creates subnet group
