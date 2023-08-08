5112882c83f81ee98f2d2c0f09ecef3d1aadf64f
spandanakoppaku <skoppaku@gmail.com>
2023-08-07 20:17:09 -0400

    Auto Push

---
+++ locals.tf
@@ -0,0 +1,7 @@
+
+locals{
+    MYSQL_USER  = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["MYSQL_USERNAME"]
+    MYSQL_PASS  = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["MYSQL_PASSWORD"]
+
+
+}
--- mysql.tf
+++ mysql.tf
@@ -3,8 +3,8 @@ resource "aws_db_instance" "mysql" {
   engine                  = "mysql"
   engine_version          = var.MYSQL_ENGINE_VERSION
   instance_class          = var.MYSQL_INSTANCE_TYPE
-  username                = "admin1"
-  password                = "roboshop1"
+  username                = locals.MYSQL_USER
+  password                = locals.MYSQL_PASS
   db_subnet_group_name    = aws_db_subnet_group.mysql_subnet_group.name
   parameter_group_name    = aws_db_parameter_group.mysql_pg.name
   skip_final_snapshot     = true
